a945294bcf0879069c913ede1afb82c4f5a78d55
lisan98 <lisandro0@hotmail.com>
2024-04-30 20:52:20 -0300

    agregue_cosas_del_2

--- src/main/java/CalculadoraDemora.java
+++ src/main/java/CalculadoraDemora.java
@@ -1,6 +1,19 @@
 import Adapters.GpsAdapter;
+import  java.util.List;
 
 public class CalculadoraDemora {
   private GpsAdapter gps;
+
+
+  public float calcularDemora(Direccion origen, List<Direccion> destino){
+    float coefDemora = 3;//demora en minutos por km
+    float distancia = gps.calcularDistancia(origen, destino.head());
+    for (int i = 0; i < destino.size() - 1; i++) {
+      int elemento1 = destino.get(i);
+      int elemento2 = destino.get(i + 1);
+      distancia += gps.calcularDistancia(elemento1, elemento2);
+    }
+    return distancia*coefDemora;
+  }
 
 }
